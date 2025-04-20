3dd39793c01d08998842396a7acbff2bda6df4d2
PremGChavan <premmernlearning@gmail.com>
2025-04-20 22:16:03 +0530

    done

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,29 +1,29 @@
-import { techStackIcons } from '../constants/index.js';
 import TitleHeader from '../components/TitleHeader';
 import TechIcon from '../components/Models/TechLogos/TechIcon.jsx';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
+import { FaReact, FaNodeJs } from "react-icons/fa";
+import { SiMongodb, SiExpress } from "react-icons/si";
 
 const TechStack = () => {
 
-  useGSAP(() => {
-    gsap.fromTo('.tech-card',
-      { 
-        y: 50 ,
-        opacity: 0 
-      },
-      {
-        y:0,
-        opacity:1,
-        duration:1,
-        ease: 'power2.inOut',
-        stagger:0.2,
-        scrollTrigger: {
-          trigger: '#skills',
-          start: 'top center'
-        }
-      }
-    )})
+  const stack = [
+    {
+      name: "MongoDB",
+      icon: <SiMongodb className="text-green-500" size={50} />,
+    },
+    {
+      name: "Express",
+      icon: <SiExpress className="text-gray-700" size={50} />,
+    },
+    {
+      name: "React",
+      icon: <FaReact className="text-blue-400 animate-spin-slow" size={50} />,
+    },
+    {
+      name: "Node.js",
+      icon: <FaNodeJs className="text-green-600" size={50} />,
+    },
+  ];
+
 
   return (
     <div id='skills' className='flex-center section-padding'>
@@ -33,22 +33,15 @@ const TechStack = () => {
           sub = "🤝 The Skills I Bring to the Table"
         />
 
-        <div className='tech-grid'>
-          {techStackIcons.map((icon) => (
-            <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
-              <div className='tech-card-animated-bg'/> 
-              <div className='tech-card-content'>
-                <div className='tech-icon-wrapper'>
-                  <TechIcon model={icon} />
-                </div>
-
-                <div className='padding-x w-full'>
-                  <p>{icon.name}</p>
-                </div>
-
-              </div>  
-            </div>
-          ))}
+        <div className="p-6 bg-black text-white-50 rounded-2xl shadow-xl max-w-md mx-auto text-center">
+          <div className="flex justify-between items-center gap-10">
+            {stack.map((tech, index) => (
+              <div key={index} className="flex flex-col items-center">
+                {tech.icon}
+                <p className="mt-2 text-sm text-gray-600">{tech.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
