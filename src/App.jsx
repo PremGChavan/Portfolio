2dde535e253c428f2ca3b22004b798e766d58bf4
PremGChavan <premmernlearning@gmail.com>
2025-04-20 21:53:55 +0530

    added nav and new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+// import LogoSection from "./sections/LogoSection"
+import NavBar from "./components/Navbar"
+import ExperienceSection from "./sections/ExperienceSection"
+import FeatureCards from "./sections/FeatureCards"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
+import TechStack from "./sections/TechStack"
 
 const App = () => {
   return (
     <>
+      <NavBar/>
       <Hero />
       <ShowcaseSection />
+      {/* <LogoSection/> */}
+      <FeatureCards/>
+      <ExperienceSection/>
+      <TechStack/>
     </>
   )
 }
